420562aac92543519f1c6f4cbe7b5ac103f1e684
MatiasCermak <matias.cermak99@gmail.com>
2021-12-20 13:53:11 -0300

    Added tab navigation and some styling

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,18 +1,47 @@
 import ElectionScreen from "../screens/ElectionScreen/ElectionScreen/ElectionScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import MainScreen from "../screens/MainScreen/MainScreen/MainScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import ProfileScreen from "../screens/ProfileScreen/ProfileScreen/ProfileScreen";
 import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator();
+const ElectionStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const ElectionStackNavigator = () => {
+    return (
+        <ElectionStack.Navigator>
+            <ElectionStack.Screen name="Home" component={MainScreen} />
+            <ElectionStack.Screen name="Election" component={ElectionScreen} />
+        </ElectionStack.Navigator>
+    );
+};
 
 const AppNavigator = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Main" component={MainScreen} />
-                <Stack.Screen name="Election" component={ElectionScreen} />
-            </Stack.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+
+                        if (route.name === "Home") {
+                            iconName = "home";
+                        } else if (route.name === "Profile") {
+                            iconName = "person";
+                        }
+
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: "blue",
+                    tabBarInactiveTintColor: "gray",
+                })}
+            >
+                <Tab.Screen options={{ headerShown: false }} name="Home" component={ElectionStackNavigator} />
+                <Tab.Screen name="Profile" component={ProfileScreen} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 };
